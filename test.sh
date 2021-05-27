faa2d40793b3f2f729eec2c4be543720475171d8
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2021-05-27 11:22:25 +0200

    Update test script for MacOS support

--- test.sh
+++ test.sh
@@ -1,22 +1,28 @@
 # Script to launch the server, webserver and all 4 clients to test
 
-./map-generator.py 32 32
+if [ "$(uname -s)" == "Darwin" ]; then
+    alias python = python3
+fi
 
-./server/server.py &
+python map-generator.py 32 32
+
+python server/server.py &
 python -m http.server 8081 --directory server/website &
 
 if [ "$(uname -s)" == "Linux" ]; then
     xdg-open http://localhost:8081/ &
     disown
+elif [ "$(uname -s)" == "Darwin" ]; then
+    open http://localhost:8081/
 else
     start http://localhost:8081/
 fi
 
 sleep 0.5
 
-./clients/bastiaan/client.py 1 &
-./clients/bastiaan/client.py 2 &
-./clients/bastiaan/client.py 3 &
-./clients/bastiaan/client.py 4
+python clients/bastiaan/client.py 1 &
+python clients/bastiaan/client.py 2 &
+python clients/bastiaan/client.py 3 &
+python clients/bastiaan/client.py 4
 
 wait < <(jobs -p)
